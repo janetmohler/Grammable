55cc7a40f270876a7917d1fadb800a846b39df25
Janet Mohler <janetmohler82@gmail.com>
2016-10-12 18:59:38 +0000

    Added destroy action to controller

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,18 +1,5 @@
 class GramsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create] 
-
-  def update 
-    @gram = Gram.find_by_id(params[:id])
-    return render_not_found if @gram.blank?
-
-    @gram.update_attributes(gram_params) 
-
-    if @gram.valid?      
-      redirect_to root_path 
-    else
-      return render :edit, status: :unprocessable_entity
-    end
-  end
   
   def new
     @gram = Gram.new
@@ -31,6 +18,19 @@ class GramsController < ApplicationController
     return render_not_found if @gram.blank?
   end
 
+  def update 
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+
+    @gram.update_attributes(gram_params) 
+
+    if @gram.valid?      
+      redirect_to root_path 
+    else
+      return render :edit, status: :unprocessable_entity
+    end
+  end
+
   def create
     @gram = current_user.grams.create(gram_params)
     if @gram.valid?
@@ -39,6 +39,13 @@ class GramsController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+
+  def destroy
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+    @gram.destroy 
+    redirect_to root_path  
+  end 
 
   private
 
